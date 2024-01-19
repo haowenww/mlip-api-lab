7a1a474126a75e86a35f349ca7a01bbe17f180ac
remywu00 <remywu@grubmarket.com>
2024-01-19 10:47:03 -0500

    export endpoint & key to environment variables

--- analyze.py
+++ analyze.py
@@ -2,9 +2,10 @@ from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from msrest.authentication import CognitiveServicesCredentials
 from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
 import time
+import os
 
-endpoint = "ENTER ENDPOINT HERE"
-key = "ENTER KEY HERE"
+endpoint = os.getenv('API_ENDPOINT')
+key = os.getenv('API_KEY')
 
 credentials = CognitiveServicesCredentials(key)
 
